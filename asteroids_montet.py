094b6b1d0d73c726682f2a8ff899b69e444ae163
Student <student@MacBook-3-1.local>
2016-06-20 09:50:15 -0400

    2nd attempt

---
+++ asteroids_montet.py
@@ -0,0 +1,14 @@
+#import day3 as d
+from day3 import add_numbers
+
+something = add_numbers(14)
+
+print something
+
+'''
+Have a module with two functions:
+1) Returns a list of near-earth asteroids
+2) Returns a dictionary of all asteroids
+
+From a different file
+Return a list of near-earth asteroids and a dictionary of all asteroids
